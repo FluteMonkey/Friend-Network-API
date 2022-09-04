34514d2e149adf56759207130ceccfa5d91996fb
Zchun <z.c.chun2003@gmail.com>
2022-09-04 17:16:10 -0500

    added findone/findoneAndDelete functions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -11,6 +11,23 @@ module.exports = {
             res.status(500).json(err);
         });
     },
+    getOneThought({ params }, res) {
+        Thought.findOne({ _id: params.id })
+        .populate([
+          { path: "reactions", select: "-__v" }
+        ])
+        .select("-__v")
+        .then((dbThoughtData) => {
+          if (!dbThoughtData) {
+            res.status(404).json({ message: "No user found with this id" });
+            return;
+          }
+          res.json(dbThoughtData);
+        })
+        .catch((err) => {
+          res.status(400).json(err);
+        });
+    },
     createThought({ body }, res) {
         Thought.create({
           thoughtText: body.thoughtText,
@@ -34,4 +51,9 @@ module.exports = {
           })
           .catch((err) => res.status(400).json(err));
       },
+      deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.id })
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => res.json(err))
+    },
 }
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,8 +3,12 @@ const router = require("express").Router();
 const {
     getAllThoughts,
     createThought,
+    getOneThought,
+    deleteThought,
 } = require("../../controllers/thought-controller");
 
 router.route("/").get(getAllThoughts).post(createThought);
+
+router.route("/:id").get(getOneThought).delete(deleteThought);
 
 module.exports = router;
