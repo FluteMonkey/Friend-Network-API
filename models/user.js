e428c0dd468adb45b5a9d42fc317cc7e2990c6e7
Zchun <z.c.chun2003@gmail.com>
2022-09-04 12:33:21 -0500

    created user model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+    type: String,
+    unique: true,
+    required: "Please enter a username",
+    trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // match validation not sure if this works
+        //match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought",
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    }, {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+});
